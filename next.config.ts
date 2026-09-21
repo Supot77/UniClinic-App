0357d2bcc81c460f444c4a659f0bb7f4b80c81f1
thanakritth <thanakrit.th@mail.wu.ac.th>
2026-09-21 20:20:38 +0700

    feat(dashboard): update role dashboards

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async redirects() {
+    return [
+      { source: '/dashboard/doctor', destination: '/dashboard/medical', permanent: true },
+      { source: '/dashboard/staff', destination: '/dashboard/staff_admin', permanent: true },
+    ];
+  },
   allowedDevOrigins: [
     '10.52.60.155',
     '10.107.0.46',
